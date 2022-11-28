c899a93b63f2f47186a75c9f07edb17d7f9c1e6e
manuel <rrmanuel1123@gmail.com>
2022-11-27 22:48:23 -0600

    CommonButton Component

---
+++ components/ButtonCommon.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nuxt-link v-if="to" :to="to" class="button">
+      <slot/>
+    </nuxt-link>
+    <button v-else class="button">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      to: {
+        type: String,
+        default: null
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .button {
+    display: flex;
+    text-decoration: none;
+    padding: 0px 8px;
+    justify-content: center;
+    align-items: center;
+    height: 32px;
+    min-width: 64px;
+    width: fit-content  ;
+    background-color: #00DC81;
+    border: none;
+    border-radius: 8px;
+    font-family: sans-serif;
+    color: white;
+    font-size: 14px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    cursor: pointer;
+  }
+
+  .button:hover {
+    background-color: #009350;
+  }
+</style>
--- components/Search.vue
+++ components/Search.vue
@@ -1,7 +1,9 @@
 <template>
 <div id="search">
     <input placeholder="Pokemon search" @keyup.enter="searchPokemon" type="text" v-model="search" />
-    <button @click="searchPokemon">Buscar</button>
+    <ButtonCommon @click="searchPokemon">Buscar</ButtonCommon>
+    <br />
+    <ButtonCommon to="/pokemon/list">Ver Lista</ButtonCommon>
 
     <div v-if="loading">
       <p>Buscando...</p>
@@ -57,18 +59,5 @@ export default Vue.extend({
     border-radius: 8px;
     text-align: center;
     margin-bottom: 8px;
-  }
-
-  button {
-    height: 32px;
-    min-width: 64px;
-    width: min-content;
-    background-color: #00DC81;
-    border: none;
-    border-radius: 8px;
-    color: white;
-    font-size: 14px;
-    font-weight: bold;
-    letter-spacing: 1px;
   }
 </style>
