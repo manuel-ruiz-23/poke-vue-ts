0683331b1d77fb419a6e1024408b7fd91f1550b5
manuel <rrmanuel1123@gmail.com>
2022-11-27 20:31:39 -0600

    Watch and computed classes with pokemon details and the list page

--- pages/pokemon/List.vue
+++ pages/pokemon/List.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
+    <div id="poke-list-page">
+        <nuxt-link to="/">Inicio</nuxt-link>
         <ul v-if="pokemons.length > 1">
-            <li v-for="pokemon in pokemons" :key="pokemon.name">
-                <nuxt-link :to="`/pokemon/${pokemon.name}`">
+            <li
+                v-for="pokemon in pokemons"
+                :key="pokemon.name"
+                @click="selectedPokemonName = pokemon.name"
+                :class="selectedPokemonName === pokemon.name ? 'selected' : ''"
+            >
+                <p>
                     {{ pokemon.name }}
-                </nuxt-link>
+                </p>
             </li>
         </ul>
         <div id="details">
-            
+            <PokeDetails v-if="selectedPokemon?.name" :pokemon="selectedPokemon" />
         </div>
     </div>
 </template>
@@ -19,36 +25,58 @@ import { PokemonType } from '~/types';
 
 export default Vue.extend({
     name: 'ListPage',
+    async asyncData({ $axios }) {
+        const response = await $axios('https://pokeapi.co/api/v2/pokemon?limit=151');
+        return { pokemons: response.data.results };
+    },
     data() {
         return {
-            pokemons: [] as { name: string, url: string}[],
+            pokemons: [] as { name: string, url: string }[],
+            selectedPokemonName: "",
             selectedPokemon: {} as PokemonType,
         }
     },
-    async asyncData({ $axios }) {
-        const response = await $axios('https://pokeapi.co/api/v2/pokemon?limit=151');
-        console.log('response', response.data.results);
-        return { pokemons: response.data.results };
+    watch: {
+        selectedPokemonName: async function () {
+            const response = await this.$axios(`https://pokeapi.co/api/v2/pokemon/${this.selectedPokemonName}`);
+            this.selectedPokemon = response.data;
+            console.log('selectedPokemon', this.selectedPokemon);
+        }
     }
 })
 </script>
 
 <style scoped>
-    ul {
-        max-height: 90vh;
-        overflow-y: scroll;
-        padding: 0;
-        max-width: 50%;
-    }
+#poke-list-page {
+    display: flex;
+    gap: 32px;
+}
 
-    li {
-        list-style: none;
-        margin-bottom: 8px;
-        border: 1px solid #ccc;
-        height: 32px;
-        padding-left: 16px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+#details {
+    flex: 1;
+}
+
+ul {
+    max-height: 90vh;
+    overflow-y: scroll;
+    padding: 0;
+    max-width: 50%;
+    flex: 1;
+}
+
+li {
+    list-style: none;
+    margin-bottom: 8px;
+    border: 1px solid #ccc;
+    height: 32px;
+    padding-left: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+}
+
+li.selected {
+    background-color: #ccc;
+}
 </style>
